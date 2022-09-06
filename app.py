11ae47452950045d008dfda39f35d8bc2aee6974
stage22devops <112963433+stage22devops@users.noreply.github.com>
2022-09-06 19:54:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hi, we have finished the deployment using the ArgoCD method !!!!'
+    return 'Hi, we have finished the deployment using the ArgoCD method !!!'
